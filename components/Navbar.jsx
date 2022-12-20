f7b766e5014c80cc3e19d1888df4f3065229d571
Kwiatek <xxkwiateqxx@gmail.com>
2022-12-20 20:57:50 +0100

    123

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -35,14 +35,7 @@ const Navbar = () => {
       <button className="min-w-[130px] py-3 text-center border border-primary bg-bgcolor rounded-2xl font-bold text-white hover:bg-primary hover:border-tr transition-all duration-600 ease-in-out xl:block hidden">
         <a href="">Contact</a>
       </button>
-      <BiMenuAltRight
-        className="text-4xl xl:hidden "
-        onClick={() => setIsOpen(true)}
-      />
-      {/* sidebar */}
-      <div className={isOpen ? "flex" : "hidden"}>
-        <div className="fixed top-0 left-0 w-full h-full bg-bgcolor z-50"></div>
-      </div>
+      <BiMenuAltRight className="text-4xl xl:hidden " />
     </div>
   );
 };
